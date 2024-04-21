c5136b444077a99e75d06a941ed6671b267cbf29
BensonGitau <bensonkamaugitau@gmail.com>
2024-04-21 23:55:58 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Home from "./Home"
+import NavBar from "./NavBar"
+import About from "./About"
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Home username="liza" city="New York"/>
       <About />
     </div>
   );
